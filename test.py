6f928059b380c9e9c0b814e1fba8851db0825445
lemwaizz <mwaidennis26@gmail.com>
2025-01-10 17:59:07 +0200

    added conditionals, randomized x better, fixed eqn

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import uvicorn 
-import numpy as np 
+import numpy as np
+import random
 
 app = FastAPI()
 
@@ -28,18 +29,24 @@ B =np.array([[1, 2, 3, 4, 5],
 def f(x):
     return np.matmul(M, x) + B
 #initialize x as a 5 * 5 matrix
-solution = [[0 for _ in range(5)] for _ in range(5)]
+x = [[random.uniform(-1, 1) for _ in range(5)] for _ in range(5) ]
 
-def without_numpy(x, y):
+def without_numpy(mat1, mat2):
+    """initialized a matrix of 0 to store solutions"""
+    solution = [[0 for _ in range(5)] for _ in range(5)]
     """nested loop to iterate through the matrices rows and columns"""
-    for i in range(5):                  #rows of M
-        for j in range(5):              #rows of B
-            for k in range(5):          #multiplication and addition
-                solution[i][j] += (x[i][k] * y[k][j])
+    if len(mat1[0]) != len(mat2):
+        raise ValueError("Invalid Matrix Dimensions")
+    else:
+        for i in range(5):                  #rows of M
+            for j in range(5):              #rows of B
+                for k in range(5):          #multiplication and addition
+                    solution[i][j] += (mat1[i][k] * x[k][j])
+                solution[i][j] += mat2[i][j]
     return solution
 
 #Make a call to the function
-solution = without_numpy(M, B)
+result = without_numpy(M, B)
 #Recreate the function with the sigmoid Function
 
 if __name__ == "__main__":
