4818b174514c95b2da8f4f912456058f9e6dd466
lemwaizz <mwaidennis26@gmail.com>
2025-01-10 17:06:52 +0200

    non-numpy solution

--- test.py
+++ test.py
@@ -27,20 +27,19 @@ B =np.array([[1, 2, 3, 4, 5],
 @app.post("/")
 def f(x):
     return np.matmul(M, x) + B
- 
-def without_numpy(x):
+#initialize x as a 5 * 5 matrix
+solution = [[0 for _ in range(5)] for _ in range(5)]
+
+def without_numpy(x, y):
     """nested loop to iterate through the matrices rows and columns"""
     for i in range(5):                  #rows of M
         for j in range(5):              #rows of B
             for k in range(5):          #multiplication and addition
-                solution[i][j] += (M[i][k] * B[k][j])
+                solution[i][j] += (x[i][k] * y[k][j])
     return solution
 
-#initialize x as a 5 * 5 matrix
-solution = [[0 for _ in range(5)] for _ in range(5)]
-
 #Make a call to the function
-without_numpy(solution)
+solution = without_numpy(M, B)
 #Recreate the function with the sigmoid Function
 
 if __name__ == "__main__":
