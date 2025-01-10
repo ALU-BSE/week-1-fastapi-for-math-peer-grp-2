bba9f0070e27348f090590fa10ba2c0031ff3a5e
Dean Daryl MURENZI <deandaryl7@gmail.com>
2025-01-10 16:29:02 +0200

    init solution with numpy

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 import uvicorn 
 import numpy as np 
+import matplotlib.pyplot as plt 
+
+
 
 app = FastAPI()
 
@@ -8,18 +11,41 @@ app = FastAPI()
 '''
     Initialize M and B as np arrays
 '''
-def f(x):
-    pass
- 
+M = np.array([[1, 2, 3, 4, 5],
+              [1, 2, 3, 4, 5],
+              [1, 2, 3, 4, 5],
+              [1, 2, 3, 4, 5],
+              [1, 2, 3, 4, 5]])
+
+B =np.array([[1, 2, 3, 4, 5],
+              [1, 2, 3, 4, 5],
+              [1, 2, 3, 4, 5],
+              [1, 2, 3, 4, 5],
+              [1, 2, 3, 4, 5]])
+
+   
 #Implement the formula MX + B
 #Have two function one using numpy and another not using numpy
 #Return 
+@app.post("/")
+def f(x):
+    return np.matmul(M, x) + B
+ 
+def without_numpy(x):
+    return "" 
 
 #initialize x as a 5 * 5 matrix
-
+X = np.ones((5, 5))
 #Make a call to the function
-
+f(X)
 #Recreate the function with the sigmoid Function
+def sigmoid(x):
+    return 1/(1 + np.exp(-x))
+
+Y = f(X)
+Z = sigmoid(Y)
+print(Y)
+print(Z.tolist())
 
 if __name__ == "__main__":
     uvicorn.run(app)
